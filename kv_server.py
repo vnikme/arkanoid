cb829143ab66f1ed211dd28ab170a290c125f859
vnikme <accs@vnik.me>
2021-04-10 10:59:35 -0400

    Basic error processing.

--- kv_server.py
+++ kv_server.py
@@ -12,14 +12,22 @@ class MyServer(BaseHTTPRequestHandler):
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
-        post_body = json.loads(post_body.decode('utf-8'))
-        key_name = post_body['key']
-        data = post_body['value']
-        globalStorage[key_name] = data
+        try:
+            post_body = json.loads(post_body.decode('utf-8'))
+        except:
+            post_body = {}
+        result = {}
+        if 'key' in post_body and 'value' in post_body:
+            key_name = post_body['key']
+            data = post_body['value']
+            globalStorage[key_name] = data
+            result['result'] = 'ok'
+        else:
+            result['result'] = 'fail'
         self.send_response(200)
-        answer = bytes(json.dumps({'result': 'ok'}), 'utf-8')
-        self.send_header("Content-type", "application/json")
-        self.send_header("Content-length", len(answer))
+        answer = bytes(json.dumps(result), 'utf-8')
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-length', len(answer))
         self.end_headers()
         self.wfile.write(answer)
 
@@ -29,9 +37,9 @@ class MyServer(BaseHTTPRequestHandler):
         answer = globalStorage.get(path, {})
         answer = bytes(json.dumps(answer), 'utf-8')
         self.send_response(200)
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Content-type", "application/json")
-        self.send_header("Content-length", len(answer))
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Content-length', len(answer))
         self.end_headers()
         self.wfile.write(answer)
 
